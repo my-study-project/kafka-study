b3ed8ea9b21a3d8074826c60b869547dfaea5304
姜爽 <2545251075@qq.com>
2022-03-29 19:44:34 +0800

    生产者消费者

---
+++ kafka-producer/src/main/java/com/js/Producer.java
@@ -0,0 +1,36 @@
+package com.js;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+import java.util.Random;
+
+public class Producer {
+    //定义主题
+    public static String topic = "duanjt_test";
+
+    public static void main(String[] args) throws InterruptedException {
+        Properties p = new Properties();
+        p.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        //kafka地址，多个地址用逗号分割
+        p.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        p.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(p);
+
+        try {
+            while (true) {
+                String msg = "Hello," + new Random().nextInt(100);
+                ProducerRecord<String, String> record = new ProducerRecord<String, String>(topic, msg);
+                kafkaProducer.send(record);
+                System.out.println("消息发送成功:" + msg);
+                Thread.sleep(500);
+            }
+        } finally {
+            kafkaProducer.close();
+        }
+
+    }
+}
